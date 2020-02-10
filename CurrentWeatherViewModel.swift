1575ef7317f76f29a193ce0e30267e55db132942
Humza Choudry <quasers101@gmail.com>
2020-02-09 22:09:37 -0500

    Add files via upload

---
+++ CurrentWeatherViewModel.swift
@@ -0,0 +1,57 @@
+// Humza Choudry
+
+import Foundation
+import UIKit
+
+struct CurrentWeatherViewModel {
+    let temperature: String
+    let humidity: String
+    let precipitationProbability: String
+    let summary: String
+    let icon: UIImage
+    
+    
+    init(model: CurrentWeather) {
+        let roundedTemperature = Int(model.temperature)
+        self.temperature = "\(roundedTemperature)º"
+        
+        let humidityPercentValue = Int(model.humidity * 100)
+        self.humidity = "\(humidityPercentValue)%"
+        
+        let precipitationPercentValue = Int(model.precipProbability * 100)
+        self.precipitationProbability = "\(precipitationPercentValue)%"
+        
+        self.summary = model.summary
+        self.icon = model.iconImage
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ViewController.swift
@@ -0,0 +1,50 @@
+// Humza Choudry
+
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var currentHumidityLabel: UILabel!
+    @IBOutlet weak var currentPrecipitationLabel: UILabel!
+    @IBOutlet weak var currentWeatherIcon: UIImageView!
+    @IBOutlet weak var currentSummaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let currentWeather = CurrentWeather(temperature: 85.0, humidity: 0.8, precipProbability: 0.1, summary: "Hot!", icon: "clear-day")
+        let viewModel = CurrentWeatherViewModel(model: currentWeather)
+        
+        displayWeather(using: viewModel)
+    }
+    
+    func displayWeather(using viewModel: CurrentWeatherViewModel) {
+        currentTemperatureLabel.text = viewModel.temperature
+        currentHumidityLabel.text = viewModel.humidity
+        currentPrecipitationLabel.text = viewModel.precipitationProbability
+        currentSummaryLabel.text = viewModel.summary
+        currentWeatherIcon.image = viewModel.icon
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
